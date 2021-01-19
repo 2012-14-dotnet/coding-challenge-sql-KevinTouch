28c29869f6bafd0adbec58ed764a849514ceae57
kevitouch0 <kevitouch0@gmail.com>
2021-01-19 08:05:29 -0800

    Fixed ddl

--- ddl.sql
+++ ddl.sql
@@ -1,27 +1,31 @@
 CREATE DATABASE EmployeeDB;
 CREATE TABLE Department
 (
-	ID INT(5),
+	ID INT(3) PRIMARY KEY,
 	Name VARCHAR(5),
-	Location VARCHAR(10)
+	Location VARCHAR(10),
+	PRIMARY KEY (ID)
+
 );
 
 CREATE TABLE Employee
 (
-	ID INT(3),
+	ID INT(3) PRIMARY KEY,
 	FirstName VARCHAR(5),
 	LastName VARCHAR(10),
 	SSN INT(9),
 	DeptID INT(5)
+		FOREIGN KEY REFERENCES Department(ID)
 );
 
 CREATE TABLE EmpDetails
 (
-	ID INT(3),
-	EmployeeID INT(3),
+	ID INT(3) PRIMARY KEY,
+	EmployeeID INT(3)
+		FOREIGN KEY REFERENCES Employee(ID),
 	Salary INT(5),
 	Address_1 INT(3),
 	City VARCHAR(10),
 	State VARCHAR(10),
-	Country VARCHAR(10)
+	Country VARCHAR(10),
 );
