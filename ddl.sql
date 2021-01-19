f319ffbdefe39c39b9902fde9c1187f154f4b89d
kevitouch0 <kevitouch0@gmail.com>
2021-01-19 08:18:10 -0800

    Changed int

--- ddl.sql
+++ ddl.sql
@@ -1,28 +1,28 @@
 CREATE DATABASE EmployeeDB;
 CREATE TABLE Department
 (
-	ID INT(3) PRIMARY KEY,
+	ID INT PRIMARY KEY,
 	Name VARCHAR(5),
 	Location VARCHAR(10),
 );
 
 CREATE TABLE Employee
 (
-	ID INT(3) PRIMARY KEY,
+	ID INT PRIMARY KEY,
 	FirstName VARCHAR(5),
 	LastName VARCHAR(10),
-	SSN INT(9),
-	DeptID INT(5)
+	SSN INT,
+	DeptID INT
 		FOREIGN KEY REFERENCES Department(ID)
 );
 
 CREATE TABLE EmpDetails
 (
-	ID INT(3) PRIMARY KEY,
-	EmployeeID INT(3)
+	ID INT PRIMARY KEY,
+	EmployeeID INT
 		FOREIGN KEY REFERENCES Employee(ID),
-	Salary INT(5),
-	Address_1 INT(3),
+	Salary INT,
+	Address_1 INT,
 	City VARCHAR(10),
 	State VARCHAR(10),
 	Country VARCHAR(10),
