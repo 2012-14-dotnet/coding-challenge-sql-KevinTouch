3c09cff7ab56cc258c5992a8d3382e8bdcffbad0
kevitouch0 <kevitouch0@gmail.com>
2021-01-19 08:14:18 -0800

    Changed double quotes to single

--- dml.sql
+++ dml.sql
@@ -1,44 +1,44 @@
 INSERT INTO Department
 VALUES
-	(1, "Tech", "Texas");
+	(1, 'Tech', 'Texas');
 INSERT INTO Department
 VALUES
-	(2, "HR", "San Francisco");
+	(2, 'HR', 'San Francisco');
 INSERT INTO Department
 VALUES
-	(3, "Marketing", "Florida");
+	(3, 'Marketing', 'Florida');
 
 
 INSERT INTO Employee
 VALUES
-	(1, "Tina", "Smith", 123456789, 2);
+	(1, 'Tina', 'Smith', 123456789, 2);
 
 INSERT INTO Employee
 VALUES
-	(2, "Kevin", "Touch", 123456789, 1);
+	(2, 'Kevin', 'Touch', 123456789, 1);
 
 INSERT INTO Employee
 VALUES
-	(3, "John", "Doe", 123456789, 3);
+	(3, 'John', 'Doe', 123456789, 3);
 
 INSERT INTO EmpDetails
 VALUES
-	(1, 1, 50000, "12345 Fake St.", "12345 Fake2 St.", "NY", "New York", "United States" );
+	(1, 1, 50000, '12345 Fake St.', '12345 Fake2 St.', 'NY', 'New York', 'United States' );
 
 INSERT INTO EmpDetails
 VALUES
-	(2, 2, 70000, "12345 Fake4 St.", "12345 Fake5 St.", "CA", "San Diego", "United States" );
+	(2, 2, 70000, '12345 Fake4 St.', '12345 Fake5 St.', 'CA', 'San Diego', 'United States' );
 
 
 INSERT INTO EmpDetails
 VALUES
-	(3, 3, 60000, "12345 Fake6 St.", "12345 Fake7 St.", "TX", "Austin", "United States" );
+	(3, 3, 60000, '12345 Fake6 St.', '12345 Fake7 St.', 'TX', 'Austin', 'United States' );
 
 SELECT *
 FROM Department
 	INNER JOIN Employee
 	ON Department.ID = Employee.ID
-HAVING Department.Name = "Marketing";
+HAVING Department.Name = 'Marketing';
 
 SELECT SUM(EmpDetails.Salary)
 FROM Department
@@ -46,7 +46,7 @@ FROM Department
 	ON Department.ID = Employee.ID
 	INNER JOIN EmpDetails
 	ON  Department.ID = EmpDetails.ID
-HAVING Department.Name = "Marketing";
+HAVING Department.Name = 'Marketing';
 
 SELECT COUNT(*)
 FROM Department
@@ -55,7 +55,7 @@ FROM Department
 GROUP BY Department;
 
 UPDATE EmpDetails SET Salary = 90000
-WHERE FirstName = "Tina" and LastName = "Smith";
+WHERE FirstName = 'Tina' and LastName = 'Smith';
 
 
 
